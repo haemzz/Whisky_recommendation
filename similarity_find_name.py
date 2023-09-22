1b67db197091c5dcfbbf5b8a595aa403047ebcb1
haemii <117583653+haemzz@users.noreply.github.com>
2023-09-22 14:19:43 +0900

    Update similarity_find_name.py

--- similarity_find_name.py
+++ similarity_find_name.py
@@ -2,15 +2,16 @@ import pandas as pd
 from fuzzywuzzy import fuzz
 
 class WhiskeyFindProcessor:
-    def __init__(self, csv_file_path, sim_length=7, threshold_similarity=60):
+    def __init__(self, csv_file_path, sim_length=7, threshold_similarity=70):
         self.df = pd.read_csv(csv_file_path, encoding='utf-8')
         self.sim_length = sim_length
         self.threshold_similarity = threshold_similarity
 
     def find_similar_names(self, name):
         temp_Sr = self.df['nameEng'].apply(str.lower)
-        similar_names = self.df[self.df['nameEng'].apply(lambda x: fuzz.partial_ratio(x, name) >= self.threshold_similarity)]
-        similar_names = similar_names.reset_index(drop=True)
+        self.df['threshold_similarity'] = temp_Sr.apply(lambda x: fuzz.partial_ratio(x, name))
+        similar_names = self.df[temp_Sr.apply(lambda x: fuzz.partial_ratio(x, name) >= self.threshold_similarity)]
+        similar_names = self.df.sort_values(by='threshold_similarity', ascending=False).reset_index(drop=True)
         return similar_names
 
     def result_list(self, input_whiskey):
